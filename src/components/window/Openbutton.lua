a6415d01871591eb0a2f8a8ec7a255d3e9d764e9
Dzxteam784 <tcandi27@gmail.com>
2025-11-15 10:32:57 +0700

    Update Openbutton.lua

--- src/components/window/Openbutton.lua
+++ src/components/window/Openbutton.lua
@@ -22,7 +22,7 @@ function OpenButton.New(Window)
     
      Icon = New("ImageLabel", {
          Image = "",
-         Size = UDim2.new(0,26,0,26),
+         Size = UDim2.new(0,50,0,50),
          Position = UDim2.new(0.5,0,0.5,0),
          LayoutOrder = -1,
          AnchorPoint = Vector2.new(0.5,0.5),
